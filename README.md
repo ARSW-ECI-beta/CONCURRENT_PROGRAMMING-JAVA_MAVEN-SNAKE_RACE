467b4badb45f5cc1adc59517443dbdd04dc2420f
cristianfmendivelso <cristianfmendi@hotmail.com>
2018-12-01 18:51:51 -0500

    Lab 2018 version

--- README.md
+++ README.md
@@ -1,49 +1,69 @@
-# snake-race-thread-concurrency
 
-![My image](./snakeraceoutput.png)
+### Escuela Colombiana de Ingeniería
+### Arquitecturas de Software - ARSW
 
+## Laboratorio - Programación concurrente, condiciones de carrera, esquemas de sincronización, colecciones sincronizadas y concurrentes.
 
-A variation of the popular game Snake using multi-snake threads moving against things to practice concurrency
+### Descripción
+Este ejercicio tiene como fin que el estudiante conozca y aplique conceptos propios de la programación concurrente.
 
-Project made during 2nd Year of graduation for the class - Concurrent and Parallel Programming
+### Parte I
 
-## Instructions
+Parte I – Antes de terminar la clase.
 
-You don't need to do anything, just watch snakes randomly battling against each other.
+Control de hilos con [wait/notify.](http://howtodoinjava.com/core-java/multi-threading/how-to-work-with-wait-notify-and-notifyall-in-java/)
 
-If you want to control a snake just Mouse-Right-Click it to select and Mouse-Left-Click in a Cell to make her move to its destiny.
+1.  Descargue el proyecto [*PrimeFinder*](https://github.com/ARSW-ECI/wait-notify-excercise).
+    Este es un programa que calcula números primos entre 0 y M
+    (Control.MAXVALUE), concurrentemente, distribuyendo la búsqueda de
+    los mismos entre n (Control.NTHREADS) hilos independientes.
 
-There are 4 Special items on the board that Snakes can interact with:
+2.  Se necesita modificar la aplicación de manera que cada t
+    milisegundos de ejecución de los threads, se detengan todos los
+    hilos y se muestre el número de primos encontrados hasta el momento.
+    Luego, se debe esperar a que el usuario presione ENTER para reanudar
+    la ejecución de los mismos. Utilice los mecanismos de sincronización
+    provistos por el lenguaje (wait y notify, notifyAll).
 
-##### Food
+Tenga en cuenta:
 
-- Grabing a rat makes your Snake grow 3 cells.
+-   La construcción synchronized se utiliza para obtener acceso exclusivo a un objeto.
 
-##### Jump Pad
+-   La instrucción A.wait() ejecutada en un hilo B pone en modo suspendido al hilo B (independientemente de qué objeto 'A' sea usado como 'lock'). Para reanudarlo, otro hilo activo puede reanudar a B haciendo 'notify()' al objeto usado como 'lock' (es decir, A).
 
-- Getting a jump pad makes your Snake able to walk through other objects/snakes.
+-   La instrucción notify(), despierta el primer hilo que hizo wait()
+    sobre el objeto.
 
-##### Barrier
-
-- Crashing against a barrier will kill your Snake
-
-##### Turbo Boost
-
-- Getting a Turbo Boost will make your Snake move 3 times as fast.
-
-Snakes finish the "race" when they reach the opposite of the Board from where they Spawned.
-
-Detailed instruction can be found in 'The Snake Race - Instrucoes.docx' & 'The Snake Race - Relatorio.docx' (both in Portuguese)
-## How to run
-
-/SnakeRace/src/snakepackage/SnakeApp.java is where the main method is. 
-
+-   La instrucción notifyAll(), despierta todos los hilos que estan
+    esperando por el objeto (hicieron wait()sobre el objeto).
 
 
-### Left to do
+### Parte II
 
-Need to refactor the code, many parts are messy. It works but it isn't very effective.
+SnakeRace es una versión autónoma, multi-serpiente del famoso juego 'snake', basado en el proyecto de João Andrade -este ejercicio es un 'fork' del mismo-. En este juego:
+	
+- N serpientes funcionan de manera autónoma.
+- No existe el concepto de colisión entre las mismas. La única forma de que mueran es estrellándose contra un muro.
+- Hay ratones distribuídos a lo largo del juego. Como en el juego clásico, cada vez que una serpiente se come a un ratón, ésta crece.
+- Existen unos puntos (flechas rojas) que teletransportan a las serpientes.
+- Los rayos hacen que la serpiente aumente su velocidad.
 
-More documentation & comments to ease cleaning code.
+![](img/sshot.png)
 
-Some OOP rules (Visibility/Encapsulation) are not being followed because it wasn't the focus on this subject, I should fix that.
+Ejercicio
+
+1. Analice el código para entender cómo hace uso de hilos para crear un comportamiento autónomo de las N serpientes.
+
+2. De acuerdo con lo anterior, y con la lógica del juego, identifique y escriba claramente (archivo RESPUESTAS.txt):
+    - Posibles condiciones de carrera.
+    - Uso inadecuado de colecciones, considerando su manejo concurrente (para esto, aumente la velocidad del juego y ejecútelo varias veces hasta que se genere un error).
+    - Uso innecesario de esperas activas.
+
+2. Identifique las regiones críticas asociadas a las condiciones de carrera, y haga algo para eliminarlas. Tenga en cuenta que se debe sincronizar estríctamente LO NECESARIO. En su documento de respuestas indique, la solución realizada para cada ítem del punto 2. Igualmente tenga en cuenta que en los siguientes puntos NO se deben agregar más posibles condiciones de carrera.
+
+3. Como se puede observar, el juego está incompleto. Haga los ajustes necesarios para que a través de botones en la interfaz se pueda Iniciar/Pausar/Reanudar el juego: iniciar el juego no se ha iniciado aún, suspender el juego si está en ejecución, reactivar el juego si está suspendido. Para esto tenga en cuenta:
+    * Al pausar (suspender) el juego, en alguna parte de la interfaz (agregue los componentes que desee) se debe mostrar:
+        - La serpiente viva más larga.
+        - La peor serpiente: la que primero murió.
+    
+        Recuerde que la suspensión de las serpientes NO es instantánea, y que se debe garantizar que se muestre información consistente.
--- src/main/java/snakepackage/Board.java
+++ src/main/java/snakepackage/Board.java
@@ -12,7 +12,8 @@ import java.util.Random;
 import javax.imageio.ImageIO;
 import javax.swing.JLabel;
 
-import enums.Grid_Size;
+import enums.GridSize;
+import java.io.InputStream;
 
 public class Board extends JLabel implements Observer {
 
@@ -27,12 +28,12 @@ public class Board extends JLabel implements Observer {
 	static Cell[] turbo_boosts = new Cell[NR_TURBO_BOOSTS];
 	static int[] result = new int[SnakeApp.MAX_THREADS];
 	Random random = new Random();
-	static Cell[][] gameboard = new Cell[Grid_Size.GRID_WIDTH][Grid_Size.GRID_HEIGHT];
+	static Cell[][] gameboard = new Cell[GridSize.GRID_WIDTH][GridSize.GRID_HEIGHT];
 
 	@SuppressWarnings("unused")
 	public Board() {
-		if ((NR_BARRIERS + NR_JUMP_PADS + NR_FOOD + NR_TURBO_BOOSTS) > Grid_Size.GRID_HEIGHT
-				* Grid_Size.GRID_WIDTH)
+		if ((NR_BARRIERS + NR_JUMP_PADS + NR_FOOD + NR_TURBO_BOOSTS) > GridSize.GRID_HEIGHT
+				* GridSize.GRID_WIDTH)
 			throw new IllegalArgumentException(); 
 		GenerateBoard();
 		GenerateFood();
@@ -43,8 +44,8 @@ public class Board extends JLabel implements Observer {
 
 	private void GenerateTurboBoosts() {
 		for (int i = 0; i != NR_TURBO_BOOSTS; i++) {
-			Cell tmp = gameboard[random.nextInt(Grid_Size.GRID_WIDTH)][random
-					.nextInt(Grid_Size.GRID_HEIGHT)];
+			Cell tmp = gameboard[random.nextInt(GridSize.GRID_WIDTH)][random
+					.nextInt(GridSize.GRID_HEIGHT)];
 			if (!tmp.hasElements()) {
 				turbo_boosts[i] = tmp;
 				turbo_boosts[i].setTurbo_boost(true);
@@ -56,8 +57,8 @@ public class Board extends JLabel implements Observer {
 
 	private void GenerateJumpPads() {
 		for (int i = 0; i != NR_JUMP_PADS; i++) {
-			Cell tmp = gameboard[random.nextInt(Grid_Size.GRID_WIDTH)][random
-					.nextInt(Grid_Size.GRID_HEIGHT)];
+			Cell tmp = gameboard[random.nextInt(GridSize.GRID_WIDTH)][random
+					.nextInt(GridSize.GRID_HEIGHT)];
 			if (!tmp.hasElements()) {
 				jump_pads[i] = tmp;
 				jump_pads[i].setJump_pad(true);
@@ -68,10 +69,10 @@ public class Board extends JLabel implements Observer {
 	}
 
 	private void GenerateBoard() {
-		for (int i = 0; i != Grid_Size.GRID_WIDTH; i++) {
-			for (int j = 0; j != Grid_Size.GRID_HEIGHT; j++) {
+		for (int i = 0; i != GridSize.GRID_WIDTH; i++) {
+			for (int j = 0; j != GridSize.GRID_HEIGHT; j++) {
 				gameboard[i][j] = new Cell(i, j);
-				System.out.println(" ins " + gameboard[i][j]);
+				//System.out.println(" ins " + gameboard[i][j]);
 			}
 		}
 
@@ -79,8 +80,8 @@ public class Board extends JLabel implements Observer {
 
 	private void GenerateBarriers() {
 		for (int i = 0; i != NR_BARRIERS; i++) {
-			Cell tmp = gameboard[random.nextInt(Grid_Size.GRID_WIDTH)][random
-					.nextInt(Grid_Size.GRID_HEIGHT)];
+			Cell tmp = gameboard[random.nextInt(GridSize.GRID_WIDTH)][random
+					.nextInt(GridSize.GRID_HEIGHT)];
 			if (!tmp.hasElements()) {
 				barriers[i] = tmp;
 				barriers[i].setBarrier(true);
@@ -92,8 +93,8 @@ public class Board extends JLabel implements Observer {
 
 	private void GenerateFood() {
 		for (int i = 0; i != NR_FOOD; i++) {
-			Cell tmp = gameboard[random.nextInt(Grid_Size.GRID_WIDTH)][random
-					.nextInt(Grid_Size.GRID_HEIGHT)];
+			Cell tmp = gameboard[random.nextInt(GridSize.GRID_WIDTH)][random
+					.nextInt(GridSize.GRID_HEIGHT)];
 			if (!tmp.hasElements()) {
 				food[i] = tmp;
 				food[i].setFood(true);
@@ -116,50 +117,48 @@ public class Board extends JLabel implements Observer {
 
 	private void drawTurboBoosts(Graphics g) {
 		Image light = null;
-		URL resource = getClass().getResource("../Img/lightning.png");
+                InputStream resource=ClassLoader.getSystemResourceAsStream("Img/lightning.png");
 		try {
 			light = ImageIO.read(resource);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 		for (Cell c : turbo_boosts) {
-			g.drawImage(light, c.getX() * Grid_Size.WIDTH_BOX, c.getY()
-					* Grid_Size.HEIGH_BOX, this);
+			g.drawImage(light, c.getX() * GridSize.WIDTH_BOX, c.getY()
+					* GridSize.HEIGH_BOX, this);
 		}
 	}
 
 	private void drawJumpPads(Graphics g) {
 		Image jump = null;
-		URL resource = getClass().getResource("../Img/up.png");
-		try {
+                
+                InputStream resource=ClassLoader.getSystemResourceAsStream("Img/up.png");
+
+                try {
 			jump = ImageIO.read(resource);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 		for (Cell c : jump_pads) {
-			g.drawImage(jump, c.getX() * Grid_Size.WIDTH_BOX, c.getY()
-					* Grid_Size.HEIGH_BOX, this);
+			g.drawImage(jump, c.getX() * GridSize.WIDTH_BOX, c.getY()
+					* GridSize.HEIGH_BOX, this);
 		}
-		// g.setColor(Color.ORANGE);
-		// for (Cell c : jump_pads) {
-		// g.fillOval(c.getX() * Grid_Size.WIDTH_BOX, c.getY()
-		// * Grid_Size.HEIGH_BOX, Grid_Size.WIDTH_BOX,
-		// Grid_Size.HEIGH_BOX);
-		// }
 	}
 
 	private void drawBarriers(Graphics g) {
 
 		Image firewall = null;
-		URL resource = getClass().getResource("../Img/firewall.png");
-		try {
+                
+                InputStream resource=ClassLoader.getSystemResourceAsStream("Img/firewall.png");
+
+                try {
 			firewall = ImageIO.read(resource);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 		for (Cell c : barriers) {
-			g.drawImage(firewall, c.getX() * Grid_Size.WIDTH_BOX, c.getY()
-					* Grid_Size.HEIGH_BOX, this);
+			g.drawImage(firewall, c.getX() * GridSize.WIDTH_BOX, c.getY()
+					* GridSize.HEIGH_BOX, this);
 			// g.fillRect(c.getX() * Grid_Size.WIDTH_BOX, c.getY()
 			// * Grid_Size.HEIGH_BOX, Grid_Size.WIDTH_BOX,
 			// Grid_Size.HEIGH_BOX);
@@ -168,7 +167,8 @@ public class Board extends JLabel implements Observer {
 
 	private void drawFood(Graphics g) {
 		Image mouse = null;
-		URL resource = getClass().getResource("../Img/mouse.png");
+                InputStream resource=ClassLoader.getSystemResourceAsStream("Img/mouse.png");
+		
 		try {
 			mouse = ImageIO.read(resource);
 		} catch (IOException e) {
@@ -176,8 +176,8 @@ public class Board extends JLabel implements Observer {
 		}
 
 		for (Cell c : food){
-		g.drawImage(mouse, c.getX() * Grid_Size.WIDTH_BOX, c.getY()
-				* Grid_Size.HEIGH_BOX, this);
+		g.drawImage(mouse, c.getX() * GridSize.WIDTH_BOX, c.getY()
+				* GridSize.HEIGH_BOX, this);
 		}
 	}
 
@@ -185,18 +185,18 @@ public class Board extends JLabel implements Observer {
 		for (int i = 0; i != SnakeApp.MAX_THREADS; i++) {
 			for (Cell p : SnakeApp.getApp().snakes[i].getBody()) {
 				if (p.equals(SnakeApp.getApp().snakes[i].getBody().peekFirst())) {
-					g.setColor(new Color(050, 205, 050));
-					g.fillRect(p.getX() * Grid_Size.WIDTH_BOX, p.getY()
-							* Grid_Size.HEIGH_BOX, Grid_Size.WIDTH_BOX,
-							Grid_Size.HEIGH_BOX);
+					g.setColor(new Color(050+(i*10), 205, 150));
+					g.fillRect(p.getX() * GridSize.WIDTH_BOX, p.getY()
+							* GridSize.HEIGH_BOX, GridSize.WIDTH_BOX,
+							GridSize.HEIGH_BOX);
 				} else {
 					if (SnakeApp.getApp().snakes[i].isSelected()) {
 						g.setColor(new Color(032, 178, 170));
 					} else
 						g.setColor(new Color(034, 139, 034));
-					g.fillRect(p.getX() * Grid_Size.WIDTH_BOX, p.getY()
-							* Grid_Size.HEIGH_BOX, Grid_Size.WIDTH_BOX,
-							Grid_Size.HEIGH_BOX);
+					g.fillRect(p.getX() * GridSize.WIDTH_BOX, p.getY()
+							* GridSize.HEIGH_BOX, GridSize.WIDTH_BOX,
+							GridSize.HEIGH_BOX);
 				}
 			}
 		}
@@ -205,18 +205,18 @@ public class Board extends JLabel implements Observer {
 
 	private void drawGrid(Graphics g) {
 		g.setColor(new Color(255, 250, 250));
-		g.fillRect(0, 0, Grid_Size.GRID_WIDTH * Grid_Size.WIDTH_BOX,
-				Grid_Size.GRID_HEIGHT * Grid_Size.HEIGH_BOX);
+		g.fillRect(0, 0, GridSize.GRID_WIDTH * GridSize.WIDTH_BOX,
+				GridSize.GRID_HEIGHT * GridSize.HEIGH_BOX);
 		g.setColor(new Color(135, 135, 135));
-		g.drawRect(0, 0, Grid_Size.GRID_WIDTH * Grid_Size.WIDTH_BOX,
-				Grid_Size.GRID_HEIGHT * Grid_Size.HEIGH_BOX);
-		for (int i = Grid_Size.WIDTH_BOX; i < Grid_Size.GRID_WIDTH
-				* Grid_Size.WIDTH_BOX; i += Grid_Size.WIDTH_BOX) {
-			g.drawLine(i, 0, i, Grid_Size.GRID_HEIGHT * Grid_Size.HEIGH_BOX);
+		g.drawRect(0, 0, GridSize.GRID_WIDTH * GridSize.WIDTH_BOX,
+				GridSize.GRID_HEIGHT * GridSize.HEIGH_BOX);
+		for (int i = GridSize.WIDTH_BOX; i < GridSize.GRID_WIDTH
+				* GridSize.WIDTH_BOX; i += GridSize.WIDTH_BOX) {
+			g.drawLine(i, 0, i, GridSize.GRID_HEIGHT * GridSize.HEIGH_BOX);
 		}
-		for (int i = Grid_Size.HEIGH_BOX; i < Grid_Size.GRID_HEIGHT
-				* Grid_Size.HEIGH_BOX; i += Grid_Size.HEIGH_BOX) {
-			g.drawLine(0, i, Grid_Size.GRID_WIDTH * Grid_Size.WIDTH_BOX, i);
+		for (int i = GridSize.HEIGH_BOX; i < GridSize.GRID_HEIGHT
+				* GridSize.HEIGH_BOX; i += GridSize.HEIGH_BOX) {
+			g.drawLine(0, i, GridSize.GRID_WIDTH * GridSize.WIDTH_BOX, i);
 		}
 
 	}
